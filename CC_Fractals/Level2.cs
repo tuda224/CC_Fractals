7357e6decda983cc2d12d9b98d47ecb27a34084b
tuda224 <johann.tuder@gmail.com>
2021-02-09 18:08:58 +0100

    initial commit

---
+++ CC_Fractals/Level2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CC_Fractals
+{
+    public static class Level2
+    {
+        public static void Run()
+        {
+            var input = ReadInput();
+        }
+
+        private static string ReadInput()
+        {
+            Console.WriteLine("Provide path to input file:");
+            var filePath = Console.ReadLine();
+            var input = File.ReadAllText(filePath);
+            return input;
+        }
+    }
+}
---
+++ CC_Fractals/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CC_Fractals
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                Level1.Run();
+                //Level2.Run();
+            }
+        }
+    }
+}
